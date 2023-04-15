70ac405373f1972b27d227abc623828f2fb83fe3
Yannis <87186644+yannis-df@users.noreply.github.com>
2023-04-15 18:23:17 +0200

    adding functions to buttons

--- dev_script.py
+++ dev_script.py
@@ -2,31 +2,46 @@
 """
 Created on Sat Apr 15 15:26:38 2023
 
-@author: yanni
+@author: yannis
 """
-## path to env: C:\Users\yanni\Documents\Code\Python\dev_project\env\Scripts\activate.bat
-#%%
 import os
 import tkinter as tk
 from tkinter import filedialog, Text
+# from tkinter import filedialog
 
-#%%
 
+# isdjfogiqjsd
 root = tk.Tk()
+
+apps = []
+def addApp():
+    filename = tk.filedialog.askopenfilename(initialdir="/", title="Select File",
+                                             filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app)
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
-frameL = tk.Frame(root, bg="white")
-frameL.place(relwidth=0.1, relheight=0.1, relx=0.1, rely=0.1)
+# frameL = tk.Frame(root, bg="white")
+# frameL.place(relwidth=0.1, relheight=0.1, relx=0.1, rely=0.1)
 
-frameR = tk.Frame(root, bg="white")
-frameR.place(relwidth=0.1, relheight=0.1, relx=0.8, rely=0.1)
+# frameR = tk.Frame(root, bg="white")
+# frameR.place(relwidth=0.1, relheight=0.1, relx=0.8, rely=0.1)
 
-frameC = tk.Frame(root, bg="white")
-frameC.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.6)
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.06)
 
-my_button = tk.Button(frameL, text="my_button", padx=10, pady=5, fg="white", bg="firebrick")
-my_button.pack()
+# my_button = tk.Button(frameL, text="my_button", padx=10, pady=5, fg="white", bg="firebrick")
+# my_button.pack()
+
+openFile = tk.Button(root, text="Open file", padx=10, pady=5, fg="white", 
+                     bg="steelblue", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="steelblue")
+runApps.pack()
 
 root.mainloop()
