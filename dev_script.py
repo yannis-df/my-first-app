fb92d09ff7f6fc70046c44991a8c5d9bcd41a7f4
Yannis <87186644+yannis-df@users.noreply.github.com>
2023-04-16 21:58:26 +0200

    Adding save file.

--- dev_script.py
+++ dev_script.py
@@ -13,33 +13,38 @@ root = tk.Tk()
 
 apps = []
 def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = tk.filedialog.askopenfilename(initialdir="/", title="Select File",
                                              filetypes=(("executables", "*.exe"), ("all files", "*.*")))
     apps.append(filename)
     print(filename)
     for app in apps:
-        label = tk.Label(frame, text=app)
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
-# frameL = tk.Frame(root, bg="white")
-# frameL.place(relwidth=0.1, relheight=0.1, relx=0.1, rely=0.1)
-
-# frameR = tk.Frame(root, bg="white")
-# frameR.place(relwidth=0.1, relheight=0.1, relx=0.8, rely=0.1)
-
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.06)
-
-# my_button = tk.Button(frameL, text="my_button", padx=10, pady=5, fg="white", bg="firebrick")
-# my_button.pack()
 
 openFile = tk.Button(root, text="Open file", padx=10, pady=5, fg="white", 
                      bg="steelblue", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="steelblue")
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", 
+                    bg="steelblue", command=runApps)
 runApps.pack()
 
 root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
