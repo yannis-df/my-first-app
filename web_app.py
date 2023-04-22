6b882e994748a8838894f84628cb0ac3ec2c9ebc
Yannis <87186644+yannis-df@users.noreply.github.com>
2023-04-22 17:47:36 +0200

    Starting web application project

---
+++ web_app.py
@@ -0,0 +1,3 @@
+## pip install -r requirements.txt  -> pour réinstaller tout les packages et avoir le même l'environnement.
+## django-admin startproject <project_name> -> créer la structure de base du projet
+## python manage.py runserver -> activer le server pour accéder au site en local.
